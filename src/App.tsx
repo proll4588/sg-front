73063e6e74df0b7c1e8c475b1f003d693e0858e0
Kostya <prol4588@gmail.com>
2024-07-13 22:20:29 +0700

    ref main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Grid,
+  Typography,
+} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { APP_NAVIGATION_MAP } from './shared/router/constants';
 
 export const App = () => {
   const navigate = useNavigate();
+
   return (
-    <Card>
-      <CardActionArea onClick={() => navigate('/user-control')}>
-        <CardContent>
-          <Typography>Управление пользователями</Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Grid>
+      <Typography
+        fontWeight={'bold'}
+        fontSize={24}
+        mb={2}
+      >
+        Модули
+      </Typography>
+      {APP_NAVIGATION_MAP.map((item) => (
+        <Card>
+          <CardActionArea onClick={() => navigate(item.url)}>
+            <CardContent sx={{ display: 'flex', gap: 2 }}>
+              {item.icon}
+              <Typography>{item.title}</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Grid>
   );
 };

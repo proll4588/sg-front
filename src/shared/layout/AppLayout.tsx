9f58352ec5ed759c883e460f03ed008438d7f33f
Kostya <prol4588@gmail.com>
2024-07-13 22:20:00 +0700

    ref app layout

--- src/shared/layout/AppLayout.tsx
+++ src/shared/layout/AppLayout.tsx
@@ -1,55 +1,249 @@
 import {
   Box,
-  AppBar,
   Toolbar,
   IconButton,
   Typography,
+  Theme,
+  CSSObject,
+  styled,
+  useTheme,
+  CssBaseline,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
   Button,
-  Grid,
 } from '@mui/material';
 import { useUser } from '../context/UserContext';
-import { Menu } from '@mui/icons-material';
-import { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import {
+  ChevronLeft,
+  ChevronRight,
+  Menu,
+  ViewQuilt,
+} from '@mui/icons-material';
+import { FC, useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import MuiDrawer from '@mui/material/Drawer';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { PAGES_URLS } from '../router/constants';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
+}));
 
 export const AppLayout: FC = ({}) => {
-  const { logout } = useUser();
+  const navigate = useNavigate();
+
+  const { pathname } = useLocation();
+
+  const { logout, user, modules } = useUser();
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Grid>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position='static'>
-          <Toolbar>
-            <IconButton
-              size='large'
-              edge='start'
-              color='inherit'
-              aria-label='menu'
-              sx={{ mr: 2 }}
-            >
-              <Menu />
-            </IconButton>
-            <Typography
-              variant='h6'
-              component='div'
-              sx={{ flexGrow: 1 }}
-            >
-              СибГУ
-            </Typography>
-            <Button
-              variant='contained'
-              color='error'
-              onClick={logout}
-            >
-              Выйти
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position='fixed'
+        open={open}
+      >
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <Menu />
+          </IconButton>
 
-      <Grid p={2}>
+          <Typography
+            variant='h6'
+            noWrap
+            component='div'
+            flexGrow={1}
+          >
+            {user ? `${user.login} | ${user.Role.title}` : '...'}
+          </Typography>
+
+          <Button
+            variant='contained'
+            color='error'
+            onClick={logout}
+          >
+            Выйти
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant='permanent'
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRight /> : <ChevronLeft />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem
+            disablePadding
+            sx={{
+              display: 'block',
+              backgroundColor:
+                pathname === '/' ? theme.palette.secondary.light : undefined,
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+              onClick={() => navigate('/')}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <ViewQuilt />
+              </ListItemIcon>
+              <ListItemText
+                primary={'Главная'}
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          {modules.map((item) => (
+            <ListItem
+              key={item.title}
+              disablePadding
+              sx={{
+                display: 'block',
+                backgroundColor:
+                  pathname === '/' + item.url
+                    ? theme.palette.secondary.light
+                    : undefined,
+              }}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+                onClick={() => navigate(PAGES_URLS.userControl)}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText
+                  primary={item.title}
+                  sx={{ opacity: open ? 1 : 0 }}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component='main'
+        sx={{ flexGrow: 1, p: 3 }}
+      >
+        <DrawerHeader />
         <Outlet />
-      </Grid>
-    </Grid>
+      </Box>
+    </Box>
   );
 };

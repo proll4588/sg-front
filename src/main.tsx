0c4ae08b59b486bd49bc4b2e5cac94ea69829b96
Kostya <prol4588@gmail.com>
2024-07-13 22:17:59 +0700

    add confirm provider

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import { ThemeProvider } from '@mui/material';
 import { theme } from './widget/theme.ts';
 import { AuthPage } from './pages/AuthPage/AuthPage.tsx';
 import { router } from './shared/router/router.tsx';
+import { ConfirmProvider } from './shared/confirm/ConfirmContext.tsx';
 
 const AuthLayout = () => {
   const { isAuth } = useUser();
@@ -26,7 +27,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ApolloProvider client={client}>
       <UserProvider>
         <ThemeProvider theme={theme}>
-          <AuthLayout />
+          <ConfirmProvider>
+            <AuthLayout />
+          </ConfirmProvider>
         </ThemeProvider>
       </UserProvider>
     </ApolloProvider>

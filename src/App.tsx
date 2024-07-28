91dbf27366245a2e197337b9e2bc18d3b53f114c
Kostya <prol4588@gmail.com>
2024-07-28 21:14:51 +0700

    fix app nav list accesses

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,18 @@ import {
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { APP_NAVIGATION_MAP } from './shared/router/constants';
+import { useUser } from './shared/context/UserContext';
 
 export const App = () => {
+  const { user } = useUser();
   const navigate = useNavigate();
 
   return (
-    <Grid>
+    <Grid
+      container
+      flexDirection={'column'}
+      gap={1}
+    >
       <Typography
         fontWeight={'bold'}
         fontSize={24}
@@ -20,7 +26,9 @@ export const App = () => {
       >
         Модули
       </Typography>
-      {APP_NAVIGATION_MAP.map((item) => (
+      {APP_NAVIGATION_MAP.filter((el) =>
+        el.access.some((el2) => el2 === user?.Role.id)
+      ).map((item) => (
         <Card>
           <CardActionArea onClick={() => navigate(item.url)}>
             <CardContent sx={{ display: 'flex', gap: 2 }}>
